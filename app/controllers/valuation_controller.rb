fa8be9581c35a6a4ec90d16d573dc77ba66f2dfc
Alex Marriott <marriottaog@gmail.com>
2021-09-22 13:50:27 +0100

    added check for correct number of card inputs

--- app/controllers/valuation_controller.rb
+++ app/controllers/valuation_controller.rb
@@ -3,6 +3,25 @@ class ValuationController < ApplicationController
   end
 
   def answer
-    @response = params["response"]
+    @cards = params["response"]
+    @cards = @cards.strip.upcase
+    if validate_input(@cards)
+      @response = 'all good'
+    else
+      @response = 'fail'
+    end
+  end
+
+  private
+
+  def validate_input(cards)
+    cards_array = cards.split(' ')
+    return false unless correct_number_of_cards?(cards_array)
+
+    true
+  end
+
+  def correct_number_of_cards?(cards_array)
+    cards_array.length == 5
   end
 end
