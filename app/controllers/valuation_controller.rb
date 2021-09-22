8268429c3b0a00f9a6a7e00a9bb2b2aa8ae6d48c
Alex Marriott <marriottaog@gmail.com>
2021-09-22 14:35:30 +0100

    card array now creates a hash early on with face and suit, makes card validation easier

--- app/controllers/valuation_controller.rb
+++ app/controllers/valuation_controller.rb
@@ -1,9 +1,12 @@
 class ValuationController < ApplicationController
+  @@valid_faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+  @@valid_suits = ['H', 'D', 'C', 'S']
+
   def input
   end
 
   def answer
-    @cards = params["response"]
+    @cards = params['response']
     @cards = @cards.strip.upcase
     if validate_input(@cards)
       @response = 'all good'
@@ -16,12 +19,34 @@ class ValuationController < ApplicationController
 
   def validate_input(cards)
     cards_array = cards.split(' ')
-    return false unless correct_number_of_cards?(cards_array)
+    cards_array.map! do |card|
+      case card.length
+      when 2
+        { face: card[0], suit: card[1] }
+      when 3
+        { face: card[0..1], suit: card[2] }
+      else
+        'input invalid'
+      end
+    end
+
+    return false if cards_array[0] == 'input invalid'
+    # return false unless correct_number_of_cards?(cards_array)
+    return false unless cards_valid?(cards_array)
 
     true
   end
 
   def correct_number_of_cards?(cards_array)
     cards_array.length == 5
+  end
+
+  def cards_valid?(cards_array)
+    cards_array.all? do |card|
+      return false if @@valid_faces.exclude?(card[:face])
+      return false if @@valid_suits.exclude?(card[:suit])
+
+      true
+    end
   end
 end
