f32903aa393fc53f66eeb7b822e4f06a06d944ba
Alex Marriott <marriottaog@gmail.com>
2021-09-23 14:32:55 +0100

    changed A value to 14, handled edge case of A 2 3 4 5 flush

--- app/controllers/valuation_controller.rb
+++ app/controllers/valuation_controller.rb
@@ -2,7 +2,6 @@ class ValuationController < ApplicationController
   @@valid_faces = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
   @@valid_suits = ['H', 'D', 'C', 'S']
   @@face_to_value_conversion = {
-    'A' => 1,
     '2' => 2,
     '3' => 3,
     '4' => 4,
@@ -14,7 +13,8 @@ class ValuationController < ApplicationController
     '10' => 10,
     'J' => 11,
     'Q' => 12,
-    'K' => 13
+    'K' => 13,
+    'A' => 14
   }
 
   def input; end
@@ -92,7 +92,7 @@ class ValuationController < ApplicationController
   end
 
   def straight?(sorted_face_value_only_cards_array)
-    sorted_face_value_only_cards_array.each_cons(2).all? { |first_card, second_card| second_card == first_card + 1 }
+    sorted_face_value_only_cards_array.each_cons(2).all? { |first_card, second_card| second_card == first_card + 1 } || sorted_face_value_only_cards_array == [1, 2, 3, 4, 5]
   end
 
   def flush?(cards_array)
