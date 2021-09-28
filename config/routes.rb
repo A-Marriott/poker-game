2570aa5b677194b44fb25930fefa0bd616beb992
Alex Marriott <marriottaog@gmail.com>
2021-09-28 19:05:43 +0100

    changed routes to have root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'input', to: 'valuation#input'
+  root to: 'valuation#input', as: :input
   get 'answer', to: 'valuation#answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
