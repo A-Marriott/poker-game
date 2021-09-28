f1c9d5b543dfd0a889c7f8d5cc823acc1cc7dac3
Alex Marriott <marriottaog@gmail.com>
2021-09-28 20:32:57 +0100

    private methods no longer take arguments but instead use instance variables

--- app/controllers/valuation_controller.rb
+++ app/controllers/valuation_controller.rb
@@ -17,9 +17,9 @@ class ValuationController < ApplicationController
       end
     end
 
-    return @response = 'Invalid hand' unless valid_input?(@cards_array)
+    return @response = 'Invalid hand' unless valid_input?
 
-    @response = rank_hand(@cards_array)
+    @response = rank_hand
   end
 
   private
@@ -44,22 +44,22 @@ class ValuationController < ApplicationController
     }
   end
 
-  def valid_input?(cards_array)
-    return false unless correct_number_of_cards?(cards_array)
-    return false unless cards_valid?(cards_array)
-    return false unless no_duplicates?(cards_array)
+  def valid_input?
+    return false unless correct_number_of_cards?
+    return false unless cards_valid?
+    return false unless no_duplicates?
 
     true
   end
 
-  def correct_number_of_cards?(cards_array)
-    cards_array.length == 5
+  def correct_number_of_cards?
+    @cards_array.length == 5
   end
 
-  def cards_valid?(cards_array)
-    return false if cards_array.any? { |card| card == 'input invalid' }
+  def cards_valid?
+    return false if @cards_array.any? { |card| card == 'input invalid' }
 
-    cards_array.all? do |card|
+    @cards_array.all? do |card|
       return false unless @valid_faces.include?(card[:face])
       return false unless @valid_suits.include?(card[:suit])
 
@@ -67,73 +67,73 @@ class ValuationController < ApplicationController
     end
   end
 
-  def no_duplicates?(cards_array)
-    cards_array.length == cards_array.uniq.length
+  def no_duplicates?
+    @cards_array.length == @cards_array.uniq.length
   end
 
-  def rank_hand(cards_array)
+  def rank_hand
     # Many functions only need the sorted face value of the card - we extract this to its own, simpler array
-    sorted_face_value_only_cards_array = cards_array.map { |card| @face_to_value_conversion[card[:face]] }.sort
+    @sorted_face_value_only_cards_array = @cards_array.map { |card| @face_to_value_conversion[card[:face]] }.sort
     # The order of these is important - we check from most valued to least valued hands, stopping if we get a match
-    return 'Straight Flush' if straight_flush?(cards_array, sorted_face_value_only_cards_array)
-    return 'Four of a Kind' if four_of_a_kind?(sorted_face_value_only_cards_array)
-    return 'Full House' if full_house?(sorted_face_value_only_cards_array)
-    return 'Straight' if straight_without_flush?(cards_array, sorted_face_value_only_cards_array)
-    return 'Flush' if flush_without_straight?(cards_array, sorted_face_value_only_cards_array)
-    return 'Three of a Kind' if three_of_a_kind?(sorted_face_value_only_cards_array)
-    return 'Two pair' if two_pair?(sorted_face_value_only_cards_array)
-    return 'One pair' if one_pair?(sorted_face_value_only_cards_array)
+    return 'Straight Flush' if straight_flush?
+    return 'Four of a Kind' if four_of_a_kind?
+    return 'Full House' if full_house?
+    return 'Straight' if straight_without_flush?
+    return 'Flush' if flush_without_straight?
+    return 'Three of a Kind' if three_of_a_kind?
+    return 'Two pair' if two_pair?
+    return 'One pair' if one_pair?
 
     'High card'
   end
 
-  def straight_flush?(cards_array, sorted_face_value_only_cards_array)
-    straight?(sorted_face_value_only_cards_array) && flush?(cards_array)
+  def straight_flush?
+    straight? && flush?
   end
 
-  def four_of_a_kind?(sorted_face_value_only_cards_array)
-    longest_number_chain(sorted_face_value_only_cards_array) == 4
+  def four_of_a_kind?
+    longest_number_chain == 4
   end
 
-  def full_house?(sorted_face_value_only_cards_array)
-    longest_number_chain(sorted_face_value_only_cards_array) == 3 && sorted_face_value_only_cards_array.uniq.length == 2
+  def full_house?
+    longest_number_chain == 3 && @sorted_face_value_only_cards_array.uniq.length == 2
   end
 
   # The or statement handles the edge case of ace being low
-  def straight?(sorted_face_value_only_cards_array)
-    sorted_face_value_only_cards_array.each_cons(2).all? { |first_card, second_card| second_card == first_card + 1 } || sorted_face_value_only_cards_array == [1, 2, 3, 4, 5]
+  def straight?
+    @sorted_face_value_only_cards_array.each_cons(2).all? { |first_card, second_card| second_card == first_card + 1 } || @sorted_face_value_only_cards_array == [1, 2, 3, 4, 5]
   end
 
-  def flush?(cards_array)
-    cards_array.all? { |card| card[:suit] == cards_array[0][:suit] }
+  def flush?
+    @cards_array.all? { |card| card[:suit] == @cards_array[0][:suit] }
   end
 
   # We need to know whether a flush is excluded, otherwise testing just for a straight could mean either a straight flush or a straight
-  def straight_without_flush?(cards_array, sorted_face_value_only_cards_array)
-    straight?(sorted_face_value_only_cards_array) && !flush?(cards_array)
+  def straight_without_flush?
+    straight? && !flush?
   end
 
-  def flush_without_straight?(cards_array, sorted_face_value_only_cards_array)
-    flush?(cards_array) && !straight?(sorted_face_value_only_cards_array)
+  def flush_without_straight?
+    flush? && !straight?
   end
 
-  def three_of_a_kind?(sorted_face_value_only_cards_array)
-    longest_number_chain(sorted_face_value_only_cards_array) == 3 && sorted_face_value_only_cards_array.uniq.length == 3
+  def three_of_a_kind?
+    longest_number_chain == 3 && @sorted_face_value_only_cards_array.uniq.length == 3
   end
 
-  def two_pair?(sorted_face_value_only_cards_array)
-    longest_number_chain(sorted_face_value_only_cards_array) == 2 && sorted_face_value_only_cards_array.uniq.length == 3
+  def two_pair?
+    longest_number_chain == 2 && @sorted_face_value_only_cards_array.uniq.length == 3
   end
 
-  def one_pair?(sorted_face_value_only_cards_array)
-    longest_number_chain(sorted_face_value_only_cards_array) == 2 && sorted_face_value_only_cards_array.uniq.length == 4
+  def one_pair?
+    longest_number_chain == 2 && @sorted_face_value_only_cards_array.uniq.length == 4
   end
 
-  def longest_number_chain(array)
-    prev_number = array[0]
+  def longest_number_chain
+    prev_number = @sorted_face_value_only_cards_array[0]
     longest_chain = 0
     current_chain = 0
-    array.each do |val|
+    @sorted_face_value_only_cards_array.each do |val|
       if prev_number == val
         current_chain += 1
         longest_chain = current_chain if current_chain > longest_chain
@@ -145,3 +145,7 @@ class ValuationController < ApplicationController
     longest_chain
   end
 end
+
+# already gotten rid of @ problems
+# but I keep calling the same method, I should just call longest number chain and uniq length once and know without having to re call
+
